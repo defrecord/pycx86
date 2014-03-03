2cd88f8b9be21b04d6ebdf81954a6f6719e0069f
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2014-03-03 00:47:23 -0800

    first version of liveness. Have to verify result

--- liveness.py
+++ liveness.py
@@ -4,6 +4,7 @@ from cvisitors import Visitor
 class Liveness(Visitor):
     def __init__(self):
         Visitor.__init__(self)
+        self.liveness_at_inst = {}
     
     def vTranslationUnit(self, node):
         print "Translation Unit"
@@ -19,7 +20,21 @@ class Liveness(Visitor):
         
     def vNodeList(self, node):
         print "Node List"
-        self._visitReverseList(node.nodes)
+        rev_list = node.nodes
+        rev_list.reverse()
+        
+        after_live = set([])
+        for inst in rev_list:
+            # Iterate through the reverse instructions
+            W, R = self.visit(inst)
+            if not W and not R:
+                continue
+            
+            print W, R
+            self.liveness_at_inst[inst] = (after_live - set(W)) | set(R)
+            after_live = self.liveness_at_inst[inst]
+            
+        print self.liveness_at_inst
         
     def vIfStatement(self, node):
         print "If"
@@ -40,10 +55,10 @@ class Liveness(Visitor):
         print "String"
         
     def vConst(self, node):
-        return node.value
+        return [None], [None]
         
     def vId(self, node):
-        return node.name
+        return [node.name]
         
     def vArrayExpression(self, node):
         print "Array"
@@ -52,10 +67,23 @@ class Liveness(Visitor):
         print "Function Call"
         
     def vReturnStatement(self, node):
-        print "Return"
+        return [], []
         
     def vBinop(self, node):
-        print "BinOp: %s %s %s" % (self.visit(node.left), node.op, self.visit(node.right))
+        W = []
+        R = []
+        if (node.op == "="):
+            W = [node.left.name]
+            nowrite, R = self.visit(node.right)
+        else:
+            mergedList = self.visit(node.left) + self.visit(node.right)
+            R = R + mergedList
+            
+        # Remove all None values from list
+        if (None in R):
+            R.remove(None)
+        
+        return W, R
         
     def vNegative(self, node):
         print "Negative"
